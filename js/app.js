90d9d4f8b7b4dbbd73f4e48bf3a46db938fd35cf
lucasjio <ljohnsen897@gmail.com>
2023-11-07 12:17:44 +0100

    added check for invalid input

--- js/app.js
+++ js/app.js
@@ -6,11 +6,14 @@ function numberInput() {
     var guess = document.querySelector("#input input").value
     console.log(guess)
     
-        if (guess > random_number) {
-            document.getElementById("console").innerHTML += guess + " er for høyt.<p>"  ;
+        if ((guess == NaN ) || (guess > 100) || (guess < 1))  {  
+            document.getElementById("console").innerHTML += "<p>Ikke gyldig. Prøv på nytt!</p>"  ;
             document.querySelector("#input input").value = NaN
         } else if (guess < random_number) {  
             document.getElementById("console").innerHTML += guess + " er for lavt.<p>"  ;
+            document.querySelector("#input input").value = NaN
+        } else if (guess > random_number) {
+            document.getElementById("console").innerHTML += guess + " er for høyt.<p>"  ;
             document.querySelector("#input input").value = NaN
         } else {  
             document.getElementById("console").innerHTML += guess + " er riktig! Vil du spille på nytt?<p><button>Spill</button>"   ;
