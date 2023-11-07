711b89d8eb2667ab2960b9c55c0a905e2bc18aa9
lucasjio <ljohnsen897@gmail.com>
2023-11-07 09:18:53 +0100

    falfkpkf

--- js/app.js
+++ js/app.js
@@ -2,17 +2,16 @@ var random_number = Math.floor(Math.random() * 101);
 var guess = 0;
 console.log(random_number);
 
-while (guess != random_number) {
- 
+// while (guess != random_number) {
     
-    if (guess > random_number) {
-        console.log("Gjett lavere!");
-    } else if (guess < random_number) {  
-        console.log("Gjett høyere!");
-    } else {  
-        console.log("Riktig!");
-        break;
-    }
-}
+//     if (guess > random_number) {
+//         console.log("Gjett lavere!");
+//     } else if (guess < random_number) {  
+//         console.log("Gjett høyere!");
+//     } else {  
+//         console.log("Riktig!");
+//         break;
+//     }
+// }
 
 
