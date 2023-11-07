3728fea3910ef59da6c3b01338e88bb9fbafaed3
DanielHansensen <danielhansendaniel@icloud.com>
2023-11-07 13:33:06 +0100

    endret på koden

--- js/app.js
+++ js/app.js
@@ -1,35 +1,35 @@
 var random_number = Math.floor(Math.random() * 101);
 var guess = 0;
-var guesses = 0;
-console.log(random_number);
+
+
+function startGame() {
+    random_number = Math.floor(Math.random() * 101);
+
+    document.getElementById("console").innerHTML = "";
+}
+
+startGame();
 
 function numberInput() {
     var guess = document.querySelector("#input input").value
-    console.log(guess)
+    
+    console.log(random_number);
+
     
         if ((guess == NaN ) || (guess > 100) || (guess < 1))  {  
             document.getElementById("console").innerHTML += "<p>Ikke gyldig. Prøv på nytt!</p>"  ;
-            document.querySelector("#input input").value = NaN
-            guesses = guesses + 1
-        } else if (guess < random_number) {  
-            document.getElementById("console").innerHTML += guess + " er for lavt.<p>"  ;
-            document.querySelector("#input input").value = NaN
-            guesses = guesses + 1
-        } else if (guess > random_number) {
-            document.getElementById("console").innerHTML += guess + " er for høyt.<p>"  ;
-            document.querySelector("#input input").value = NaN
-            guesses = guesses + 1
-        } else {  
-            document.getElementById("console").innerHTML += guess + " er riktig! Vil du spille på nytt?<p><button>Spill</button>"   ;
             document.querySelector("#input input").value = ""
-            guesses = guesses + 1
+        } else if (guess < random_number) {  
+            document.getElementById("console").innerHTML += "<p>"  + guess + " er for lavt.</p>";
+            document.querySelector("#input input").value = ""
+        } else if (guess > random_number) {
+            document.getElementById("console").innerHTML += "<p>" + guess + " er for høyt.</p>"  ;
+            document.querySelector("#input input").value = ""
+        } else {  
+            document.getElementById("console").innerHTML += "<p>" + guess + " er riktig! Vil du spille på nytt?</p><button onclick='startGame()''>Spill</button>"   ;
+            document.querySelector("#input input").value = ""
         }
     }
-
-function resetGame () {
-    document.getElementById("console").innerHTML = "";
-
-}
 
 
 
