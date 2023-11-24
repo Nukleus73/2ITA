3dc2b4a6933569c87c7f6c51ce9d1c14a39d6821
lucasjio <ljohnsen897@gmail.com>
2023-11-24 12:19:38 +0100

    80% finished with logi

--- Quiz/script2.js
+++ Quiz/script2.js
@@ -7,12 +7,13 @@ function checkAnswer(selectedOption) {
         alert("Riktig svar!");
     } else {
         alert("Feil svar. Prøv igjen!");
-        return // Ikke gå videre til neste spørsmål ved feil svar
+        return; // Ikke gå videre til neste spørsmål ved feil svar
     }
 
     currentQuestion++;
     if (currentQuestion < questions.length) {
         updateUI();
+        updateProgressBar();
     } else {
         alert("Quiz ferdig!");
         resetQuiz();
@@ -25,13 +26,20 @@ function updateUI() {
 
     questionElement.textContent = questions[currentQuestion].question;
     options.forEach((option, index) => {
-        option.textContent = questions[currentQuestion].options[index];
+        option.lastElementChild.textContent = questions[currentQuestion].options[index];
     });
 }
 
 function resetQuiz() {
     currentQuestion = 0;
     updateUI();
+    updateProgressBar(); // Reset progress bar
+}
+
+function updateProgressBar() {
+    const progressBar = document.getElementById("bar");
+    const progress = ((currentQuestion + 1) / questions.length) * 100;
+    progressBar.style.width = `${progress}%`;
 }
 
 const questions = [
@@ -53,6 +61,21 @@ const questions = [
     {
         question: "Hva er hovedstaden i Finland?",
         options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 3
+    },
+    {
+        question: "Hva er hovedstaden i Sverige?",
+        options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 1
+    },
+    {
+        question: "Hva heter jeg?",
+        options: ["Lucas", "Nichlas", "Daniel", "Edvin"],
+        correctAnswer: 0
+    },
+    {
+        question: "Bing chilling?",
+        options: ["Chillern", "Mango", "Bing Chlling", "Eple"],
         correctAnswer: 3
     },
     // Legg til flere spørsmål etter behov
