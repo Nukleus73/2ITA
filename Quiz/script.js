a426f06e309951f394928642a92ab1f6bf381ab9
DanielHansensen <danielhansendaniel@icloud.com>
2023-11-24 11:12:24 +0100

    added mute function

--- Quiz/script.js
+++ Quiz/script.js
@@ -1,3 +1,15 @@
-function answer() {
+let sound = true;
+
+function muteSound() {
     
+    if(sound == true) {
+        sound = false;
+
+        document.querySelector("#volumeButton").setAttribute("name", "volume-mute-outline");
+    }
+    else if(sound == false) {
+        sound = true;
+
+        document.querySelector("#volumeButton").setAttribute("name", "volume-high-outline");
+    }
 }
