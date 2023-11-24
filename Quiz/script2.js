3b8048021cec660c84a378a66ef52505bdbe29e3
lucasjio <ljohnsen897@gmail.com>
2023-11-24 11:41:07 +0100

    changes onClick function in Html

---
+++ Quiz/script2.js
@@ -0,0 +1,61 @@
+let currentQuestion = 0;
+
+function checkAnswer(selectedOption) {
+    const correctAnswerIndex = questions[currentQuestion].correctAnswer;
+
+    if (selectedOption.textContent === questions[currentQuestion].options[correctAnswerIndex]) {
+        alert("Riktig svar!");
+    } else {
+        alert("Feil svar. Prøv igjen!");
+        return // Ikke gå videre til neste spørsmål ved feil svar
+    }
+
+    currentQuestion++;
+    if (currentQuestion < questions.length) {
+        updateUI();
+    } else {
+        alert("Quiz ferdig!");
+        resetQuiz();
+    }
+}
+
+function updateUI() {
+    const questionElement = document.getElementById("question");
+    const options = document.querySelectorAll(".option");
+
+    questionElement.textContent = questions[currentQuestion].question;
+    options.forEach((option, index) => {
+        option.textContent = questions[currentQuestion].options[index];
+    });
+}
+
+function resetQuiz() {
+    currentQuestion = 0;
+    updateUI();
+}
+
+const questions = [
+    {
+        question: "Hva er hovedstaden i Norge?",
+        options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 0
+    },
+    {
+        question: "Hva er hovedstaden i Sverige?",
+        options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 1
+    },
+    {
+        question: "Hva er hovedstaden i Danmark?",
+        options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 2
+    },
+    {
+        question: "Hva er hovedstaden i Finland?",
+        options: ["Oslo", "Stockholm", "København", "Helsinki"],
+        correctAnswer: 3
+    },
+    // Legg til flere spørsmål etter behov
+];
+
+updateUI();
