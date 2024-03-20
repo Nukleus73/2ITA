c723c19697841d485ab443df1c0d96d51ea172de
lucasjio <ljohnsen897@gmail.com>
2024-03-20 12:05:15 +0100

    added visual studio code course innhold

--- Kurs i nettside/script/JSON-CMS.js
+++ Kurs i nettside/script/JSON-CMS.js
@@ -36,3 +36,52 @@ function JSON_CMS(JSON_File, sendToObject) {
         sendToObject.innerHTML = coursesHTML;
       });
 }
+
+
+function JSON_CMS(JSON_File, sendToObject) {
+  // henter ut .JSON filen og bruker den i funksjonen
+  readJSON(JSON_File, (error, data) => {
+
+      // gjør dataen tilgjenelig
+      var coursesHTML = ''; 
+      var course = data;
+
+      // Generate HTML for course
+      coursesHTML += `<h2>${course.tittel}</h2>`;
+      coursesHTML += `<p>${course.intro}</p>`;
+
+      // Loop through leksjoner array
+      course.leksjoner.forEach(leksjon => {
+          // Generate HTML for each leksjon
+          coursesHTML += `<div class="lesson" style="display: none;">
+                              <h3>${leksjon.tittel}</h3>
+                              <p>${leksjon.intro}</p>`;
+
+          if (leksjon.shortcuts) {
+              // Generate HTML for shortcuts
+              coursesHTML += `<ul>`;
+              leksjon.shortcuts.forEach(shortcut => {
+                  coursesHTML += `<li>${shortcut.kommando}: ${shortcut.funksjon}</li>`;
+              });
+              coursesHTML += `</ul>`;
+          }
+
+          if (leksjon.extensions) {
+              // Generate HTML for extensions
+              coursesHTML += `<ul>`;
+              leksjon.extensions.forEach(extension => {
+                  coursesHTML += `<li>${extension.navn}: ${extension.beskrivelse}</li>`;
+              });
+              coursesHTML += `</ul>`;
+          }
+
+          // Close leksjon div
+          coursesHTML += `</div>`;
+      });
+
+      // Insert formatted HTML into the courses div
+      sendToObject.innerHTML = coursesHTML;
+  });
+}
+
+
