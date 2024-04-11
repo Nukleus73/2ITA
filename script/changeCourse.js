04fb659127a275cc4f2623670c4277b708f3b7d3
daniel-ihansen <danielhansendaniel@icloud.com>
2024-04-11 14:55:22 +0200

    omgjorde jsonToHtml, la inn CSS kurset, la inn JS kurset, la inn bilder

---
+++ script/changeCourse.js
@@ -0,0 +1,79 @@
+function changeCourse(course) {
+  //  nar du bytter kurs, skal leksjoner ga til null
+  currentLesson = 0;
+
+  const root = document.documentElement;
+  const style = getComputedStyle(root);
+  let variableValue;
+
+  const navLinks = document.querySelectorAll(".courseButton");
+  navLinks.forEach((link) => {
+    if (link.classList.contains("active")) {
+      link.classList.remove("active");
+    }
+  });
+
+  document.querySelector(".courses").innerHTML = "";
+
+  switch (course) {
+    case "VSC":
+      // Retrieve the value of the specified CSS variable
+      variableValue = style.getPropertyValue(`--accentVSC`).trim();
+
+      navLinks[0].classList.add("active");
+
+      //  henter .json filen og genererer et kurs i #course
+      jsonToHTML("./courses/VSC.json", document.querySelector(".courses"));
+      break;
+
+    case "CSS":
+      // Retrieve the value of the specified CSS variable
+      variableValue = style.getPropertyValue(`--accentCSS`).trim();
+
+      navLinks[1].classList.add("active");
+
+      //  henter .json filen og genererer et kurs i #course
+      jsonToHTML("./courses/CSS.json", document.querySelector(".courses"));
+      break;
+
+    case "JS":
+      // Retrieve the value of the specified CSS variable
+      variableValue = style.getPropertyValue(`--accentJS`).trim();
+
+      navLinks[2].classList.add("active");
+
+      jsonToHTML("./courses/JS.json", document.querySelector(".courses"));
+      break;
+
+    case "PHP":
+      // Retrieve the value of the specified PHP variable
+      variableValue = style.getPropertyValue(`--accentPHP`).trim();
+
+      navLinks[3].classList.add("active");
+
+      jsonToHTML("./courses/PHP.json", document.querySelector(".courses"));
+      break;
+  }
+
+  setTimeout(() => {
+    //  start med den første leksjonen
+    if (document.querySelectorAll(".lesson").length) {
+      document.querySelectorAll(".lesson")[0].style.display = "block";
+
+      //  oppdater kapittellisten
+      document.querySelectorAll("#courseNav h4 span")[1].innerHTML =
+        document.querySelectorAll(".lesson").length;
+      document.querySelectorAll("#courseNav h4 span")[0].innerHTML = 1;
+    } else {
+      //  om det ikke er noe kapittel, er den blank
+      document.querySelectorAll("#courseNav h4 span")[1].innerHTML = "-";
+      document.querySelectorAll("#courseNav h4 span")[0].innerHTML = "-";
+    }
+
+    document.querySelectorAll("#lessonList h5")[0].classList.add("active");
+  }, 30);
+  document.querySelector("#lessonList").innerHTML = "";
+
+  // Set the value of variable --blue to another value (in this case "lightblue")
+  root.style.setProperty("--accentMain", variableValue);
+}
---
+++ script/jsonToHTML.js
@@ -0,0 +1,137 @@
+//  funksjon for å konvertere en json fil til html kode i et bestemt format.
+function jsonToHTML(jsonPath, parentElement) {
+  //  async = vent på alt med "await", så gjør resten av koden
+  (async () => {
+    //  hent ut .JSON filen
+    const course = await fetchJSON(jsonPath);
+
+    //  logg hentingen
+    console.log(`[jsonToHTML.js]: Successfully fetched (${jsonPath})`);
+
+    course.lessons.forEach((lesson) => {
+      let lessonTitle = document.createElement("h5");
+      lessonTitle.innerHTML = `${lesson.title}`;
+      document.querySelector("#lessonList").appendChild(lessonTitle);
+    });
+
+    //  for hver leksjon
+    course.lessons.forEach((lesson) => {
+      //  lag en kontainer for leksjonen
+      let lessonContainer = document.createElement("div");
+      lessonContainer.classList.add("lesson");
+
+      // lag en "h3" tag med tittel, fyll den og send den til siden
+      let title = document.createElement("h3");
+      title.innerHTML = `${lesson.title}`;
+      lessonContainer.appendChild(title);
+
+      //  for hvert element i leksjonen
+      lesson.content.forEach((content) => {
+        let section = document.createElement("section")
+        content.section.forEach((element) => {
+
+          //  om elementet er et bilde
+          if (element.image) {
+            // lag en "figure" tag som en figur
+            let figure = document.createElement("figure");
+
+            // lag en "img" tag med bildet, fyll den og send den til siden
+            let img = document.createElement("img");
+            img.src = `./media/bilder/${element.image}`;
+            img.alt = element.caption;
+            figure.appendChild(img);
+
+            if (element.caption) {
+              // lag en "figcaption" tag med tekst, fyll den og send den til siden
+              let caption = document.createElement("figcaption");
+              caption.innerHTML = element.caption;
+              figure.appendChild(caption);
+            }
+
+            //  om bildet er et landskapsbilde, har det storre bredde
+            if (element.landscape) {
+              figure.classList.add("landscape");
+            }
+
+            section.appendChild(figure);
+          }
+
+          //  om innholdet er en tekst
+          if (element.paragraph) {
+            // lag en "p" tag med teksten, fyll den og send den til siden
+            let paragraph = document.createElement("p");
+            paragraph.innerHTML = element.paragraph;
+            section.appendChild(paragraph);
+          }
+
+          //  om innholdet er en link
+          if (element.link) {
+            // lag en "img" tag med bildet, fyll den og send den til siden
+            let link = document.createElement("a");
+            link.href = element.link;
+            link.target = "_blank"
+            link.innerHTML = element.tag;
+            section.appendChild(link);
+          }
+
+          // om det eksisterer en video i json filen
+          if (element.video) {
+            // lag en "video" tag med video, fyll den og send den til siden
+            let video = document.createElement("video");
+            video.src = element.video;
+            video.controls = true;
+            section.appendChild(video);
+          }
+
+          if (element.code) {
+            let code = document.createElement("code");
+
+            if (element.space) {
+                code.style.width = "100%";
+            }
+
+            let codeLetters = element.code.split(''); // Split code into lines
+            let finishedLetters = '';
+            // Indentation level tracker
+
+            // Loop through each letter of the code
+            codeLetters.forEach(letter => {
+              if (letter == '{' || letter == ';') {
+                letter += '<br>';
+              }
+              if (letter == '|') {
+                letter = '<br>';
+              }
+              finishedLetters += letter
+            });
+            code.innerHTML = finishedLetters
+            section.appendChild(code);
+          }
+
+          lessonContainer.appendChild(section);
+        });
+      });
+
+      //  om det eksisterer oppgaver i json filen
+      if (lesson.tasks) {
+        //  lag tittel "oppgaver"
+        let title = document.createElement("h3");
+        title.innerHTML = "Oppgaver";
+        lessonContainer.appendChild(title);
+
+        //  teller antall oppgaver
+        let taskCounter = 0;
+        //  for hvert oppgave
+        lesson.tasks.forEach((task) => {
+          taskCounter++;
+
+          // lag en "p" tag med oppgaven, fyll den og send den til siden
+          let tas = document.createElement("p");
+          tas.innerHTML = `<span>Oppgave ${taskCounter}</span>: ${task.task}`;
+          lessonContainer.appendChild(tas);
+        });
+      }
+      parentElement.appendChild(lessonContainer);
+    });
+  })();
+}
