154c6ead79852ae719e491324c64b51a07afaa41
daniel-ihansen <danielhansendaniel@icloud.com>
2024-04-11 16:05:02 +0200

    lager ny leksjon i css

--- Kurs i nettside/script/fetchJSON.js
+++
@@ -1,7 +0,0 @@
-// Function to fetch and read JSON file
-async function fetchJSON(jsonFile) {
-  const response = await fetch(jsonFile);
-  const jsonData = await response.json();
-
-  return jsonData;
-}
